5a38b2b8aba24a66f1a920df4196fe4c70e72bad
MohsenJadidi <47446864+MohsenJadidi@users.noreply.github.com>
2019-11-03 23:06:11 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 from multiprocessing import Process, Manager
 import time
+from random import randrange
 from readFile import readFile
 
 N = 1 # input port
 input_rate = int(45e6) # input transmision capacity
 C = [15, 25, 35] # outgoing rate(per second)
 C = 25
-B = 10 # Buffer size 
+B = 1000 # Buffer size 
 num_frame = 100
 
 class Packet:
     i = 0
-    def __init__(self):
+    def __init__(self,lenght,num):
         self.time_process = 0
         self.birth_time = time.monotonic()
+        self.size = lenght
+        self.frameNumber = num
         self.string = 'Packet'+ str(Packet.i)
         Packet.i = Packet.i + 1
     
@@ -27,35 +30,67 @@ def display(L):
         print(i,end=' ')
     print(']')    
 
-def arrive(L, V):  # the managed list `L` passed explicitly.
+def arrive(L, V, PLR):  # the managed list `L` passed explicitly.
     print('L1')
     print(V)
+    total_packets = 0
+    loss_packets = 0
     for i in range(len(V)):
         time.sleep(1/25)
         if len(L) < B: 
             print('Frame is recieved!')
-            #packets = Frame_to_Packets(V(i))
-            L.insert(0,Packet())
-            print('L:',end=' ')
-            display(L)
+            packets = Frame_to_Packets(V[i],i)
+            total_packets = total_packets + len(packets)
+            if (len(L))+(len(packets)) <= B:
+                L[0:0]=packets              # insert packets to L element by element
+                #print('L:',end=' ')
+                #display(L)
+            else:
+                loss_packets = loss_packets + len(packets)
+                
+    PLR.append(total_packets)
+    PLR.append(loss_packets)
+    PLR.append(loss_packets/total_packets)
+    
 
 def depart(L, i):  # the managed list `L` passed explicitly.
     print('L2')
     num_buffer_zero = 0
+    r = 75  # r packet per 1 sec
     while True:
-        time.sleep(2/25)
+        time.sleep(1/r)
         if len(L) > 0:
-            print('Frame is departed!')
+            print('Packet is departed!')
             L.pop()
-            print('L:',end=' ')
-            display(L)
+            #print('L:',end=' ')
+            #display(L)
+            print('***********  L = ', len(L) ,'    ***********')
             num_buffer_zero = 0
         else:
             num_buffer_zero = num_buffer_zero + 1
             if num_buffer_zero > 25:
                 break;
 
-#def Frame_to_Packets:
+def Frame_to_Packets(bits,frameNumber):
+    min_packet_size = 80
+    bits_res = bits
+    packets = []
+    size = (min_packet_size + randrange(0,40)) * 8 # 8 for byte to bit
+    packets.insert(0,Packet(size+(20*8), frameNumber))        # 20*8 for IP header
+    bits_res = bits_res - size
+    while bits_res > 0:
+        size = (min_packet_size + randrange(0,40)) * 8
+        if size > bits_res:
+            packets.insert(0,Packet(bits_res+(20*8), frameNumber))
+        else:
+            packets.insert(0,Packet(size+(20*8), frameNumber))
+        bits_res = bits_res - size 
+        
+    return packets        
+        
+    
+        
+    
            
             
 
@@ -73,8 +108,9 @@ if __name__ == "__main__":
     '''
     with Manager() as manager:
         L = manager.list()  # <-- can be shared between processes.
+        PLR = manager.list()
         processes = []
-        p1 = Process(target=arrive, args=(L,V1))  # Passing the list
+        p1 = Process(target=arrive, args=(L, V1, PLR))  # Passing the list
         p1.start()
         processes.append(p1)
         p2 = Process(target=depart, args=(L,1))  # Passing the list
@@ -83,22 +119,7 @@ if __name__ == "__main__":
         for p in processes:
             p.join()
         print(L)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print(PLR)
 
 
 
